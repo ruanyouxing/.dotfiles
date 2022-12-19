328615c0524d46a5000939b3adfbbc8425a257fb
ruanyouxing <ruanyouxing7@gmail.com>
2022-12-19 14:10:27 +0700

    zplug

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ sh -c 'sudo pacman -S --needed - < pacman.txt'
 paru -S --needed - < aur.txt
 stow --target='/home/hungz' . 
 git clone https://github.com/ruanyouxing/nvim ~/.config/nvim
+git clone https://github.com/zplug/zplug ~/.zplug
