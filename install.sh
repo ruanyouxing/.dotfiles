2ef2cb43ee81272152bc987f9a1e28c736535108
ruanyouxing <ruanyouxing7@gmail.com>
2023-08-03 10:22:45 +0700

    h

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-for path in .config/*; do ln -s "${HOME:?}/.dotfiles/$path" "${HOME:?}/$path"; done
-ln -s ~/.dotfiles/.config/picom.conf ~/.config/picom.conf
+# for path in .config/*; do ln -s "${HOME:?}/.dotfiles/$path" "${HOME:?}/$path"; done
+if [ ! -d '/home/hungz/.config/home-manager/' ]; then
+  ln -s "${HOME:?}/.dotfiles/home-manager" "${HOME:?}/.config/home-manager"
+fi
