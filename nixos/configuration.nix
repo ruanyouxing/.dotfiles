b3f7cab608b2fe144b5cbd5fe6aba35674d280a7
ruanyouxing <ruanyouxing7@gmail.com>
2023-08-03 22:37:08 +0700

    time synchronization

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,18 +1,12 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   config,
   pkgs,
   ...
 }: {
   imports = [
-    # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
-
-  # Bootloader.
-  boot.kernelParams = ["quiet" "splash"];
+  boot.kernelParams = ["quiet" "splash" "rd.udev.log_priority=3"];
   boot.loader.systemd-boot = {
     enable = true;
     consoleMode = "auto";
@@ -42,10 +36,11 @@
     videoDrivers = ["nvidia"];
   };
   services.printing.enable = true;
+  services.blueman.enable = true;
+  services.chrony.enable = true;
   sound.enable = true;
   hardware.enableAllFirmware = true;
   hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
   hardware = {
     opengl = {
       enable = true;
@@ -77,53 +72,32 @@
     extraGroups = ["networkmanager" "wheel"];
     packages = with pkgs; [
       firefox
-      kate
-      #  thunderbird
     ];
   };
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "hungz";
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    vim
     wget
     python3
     killall
     python311Packages.dbus-python
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
   # programs.mtr.enable = true;
   # programs.gnupg.agent = {
   #   enable = true;
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   security.sudo.extraRules = [
     {
       users = ["hungz"];
       commands = [
         {
           command = "ALL";
-          options = ["NOPASSWD"]; # "SETENV" # Adding the following could be a good idea
+          options = ["NOPASSWD"];
         }
       ];
     }
@@ -139,5 +113,5 @@
       options = "--delete-older-than 7d";
     };
   };
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system.stateVersion = "23.05";
 }
