f64573c2c3d23f01973a19b99a1cbe37493cde5b
ruanyouxing <ruanyouxing7@gmail.com>
2023-08-27 20:18:54 +0700

    zsh zinit plugin

--- home-manager/shellcfg.nix
+++ home-manager/shellcfg.nix
@@ -5,36 +5,19 @@
     enable = true;
     enableAutosuggestions = true;
     enableCompletion = true;
-    plugins = [
-      {
-        name = "fast-syntax-highlighting";
-        src = pkgs.fetchFromGitHub {
-          owner = "zdharma-continuum";
-          repo = "fast-syntax-highlighting";
-          rev = "cf318e06a9b7c9f2219d78f41b46fa6e06011fd9";
-          hash = "sha256-RVX9ZSzjBW3LpFs2W86lKI6vtcvDWP6EPxzeTcRZua4=";
-        };
-      }
-
-      {
-        name = "zsh-autopair";
-        src = pkgs.fetchFromGitHub {
-          owner = "hlissner";
-          repo = "zsh-autopair";
-          rev = "396c38a7468458ba29011f2ad4112e4fd35f78e6";
-          sha256 = "PXHxPxFeoYXYMOC29YQKDdMnqTO0toyA7eJTSCV6PGE=";
-        };
-      }
-      {
-        name = "k";
-        src = pkgs.fetchFromGitHub {
-          owner = "supercrabtree";
-          repo = "k";
-          rev = "e2bfbaf3b8ca92d6ffc4280211805ce4b8a8c19e";
-          sha256 = "32rJjBzqS2e6w/L78KMNwQRg4E3sqqdAmb87XEhqbRQ=";
-        };
-      }
-    ];
+    initExtraFirst = ''
+      source ${pkgs.zinit}/share/zinit/zinit.zsh
+      zinit load hlissner/zsh-autopair
+      zinit wait lucid for \
+      atinit"ZINIT[COMPINIT_OPTS]=-C; zicompinit; zicdreplay" \
+        zdharma-continuum/fast-syntax-highlighting \
+      blockf \
+        zsh-users/zsh-completions \
+      atload"!_zsh_autosuggest_start" \
+        zsh-users/zsh-autosuggestions
+      zinit ice as"command" from"gh-r" \
+      atpull"%atclone" src"init.zsh"
+    '';
     initExtra = ''
       cd(){
           if (( $# == 0  ))
