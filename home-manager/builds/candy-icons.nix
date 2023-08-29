5b1dcef5c497633ae707692c0e901d439f9f75cc
ruanyouxing <ruanyouxing7@gmail.com>
2023-08-30 02:16:29 +0700

    remove comments

--- home-manager/builds/candy-icons.nix
+++ home-manager/builds/candy-icons.nix
@@ -4,9 +4,6 @@
     src = fetchFromGitHub {
       owner = "EliverLara";
       repo = "candy-icons";
-      # repo = "Sweet-folders";
-      # rev = "b2192ff1412472f036fdf9778c6b9dbcb6c044ec";
-      # hash = "sha256-QexfqXH5a1IEhKBRjWSMdrEvThvLRzd4M32Xti1DCGE=";
       rev = "f7f19a1fa167d7799682f4f9fb2249271fc8efa8";
       hash = "sha256-g+0eTweLsPG4g7X0wOuNFzjZug8R+vnhO6VBnAu/Yzw=";
     };
