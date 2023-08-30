31e8cebe57fbbbece4d95e05a5f252b3364a8962
ruanyouxing <ruanyouxing7@gmail.com>
2023-08-31 01:10:46 +0700

    extract tarball

--- home-manager/builds/sweet-cursors.nix
+++ home-manager/builds/sweet-cursors.nix
@@ -4,9 +4,9 @@
 }:
 stdenv.mkDerivation {
   name = "sweet-cursors";
-  src = ./Sweet-cursors.tar.xz;
+  src = ./Sweet-cursors;
   installPhase = ''
-  mkdir -p $out/share/icons/
-  tar -xf $src -C $out/share/icons
+  mkdir -p $out/share/icons/Sweet-cursors
+  cp -r $src/* $out/share/icons/Sweet-cursors/
   '';
 }
