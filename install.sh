a1cf6991b30edf4432081b8fcb43b29b06afd794
ruanyouxing <ruanyouxing7@gmail.com>
2023-09-01 16:48:05 +0700

    install scripts

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 sh -c 'sudo pacman -Syyu'
 sh -c 'sudo pacman -S --needed - < pacman.txt'
-paru -S --needed - < aur.txt
-cp -vr ./scripts ~/.local/bin/
-stow --target='/home/hungz' . 
+yay -S --needed - < aur.txt
+ln -sf "/home/hungz/.dotfiles/scripts" "${HOME}"/.local/bin/
+cd ./.config/ 
+for conf_dirs in *; do 
+  ln -sf "/home/hungz/.dotfiles/.config/$conf_dirs" "/home/hungz/.config/$conf_dirs";
+  if [ -L "/home/hungz/.dotfiles/$conf_dirs/$conf_dirs" ]; then
+    unlink "/home/hungz/.dotfiles/$conf_dirs/$conf_dirs"
+  fi
+done
 git clone https://github.com/ruanyouxing/nvim ~/.config/nvim
