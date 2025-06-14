d5a9536f826c87f4be053198c067be2519955267
ruanyouxing <ruanyouxing7@gmail.com>
2025-06-14 11:49:15 +0700

    h

--- home-manager/shellcfg.nix
+++ home-manager/shellcfg.nix
@@ -46,8 +46,8 @@ in {
               fi
             }
 
-      if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then
-        exec Hyprland
+      if uwsm check may-start; then
+        exec uwsm start hyprland-uwsm.desktop
       fi
     '';
   };
