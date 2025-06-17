a5810760486562d19e6bbb880205cd45ab2b3efd
ruanyouxing <ruanyouxing7@gmail.com>
2025-06-17 16:27:13 +0700

    first quickshell test

--- home-manager/gui/quickshell/default.nix
+++ home-manager/gui/quickshell/default.nix
@@ -1,5 +1,10 @@
-{inputs,...}:{
+{
+  inputs,
+  config,
+  ...
+}: {
   home.packages = [
     inputs.quickshell.packages.x86_64-linux.default
   ];
+  xdg.configFile."quickshell/shell.qml".source = (toString ./shell.qml);
 }
