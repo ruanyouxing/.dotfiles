f75386f68a7e4e6069e14b1df4d2b81b11bd6744
ruanyouxing <ruanyouxing7@gmail.com>
2025-09-07 17:25:00 +0700

    change install script

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
-
-if [ ! -d "${HOME}/.config/home-manager/" ]; then
-  ln -s "${HOME:?}/.dotfiles/home-manager" "${HOME:?}/.config/home-manager"
-fi
 if [ ! -d "${HOME}/.config/nvim" ]; then
   git clone https://github.com/ruanyouxing/nvim ~/.config/nvim 
 fi
 
 nixos-generate-config --show-hardware-config > ./nixos/hardware-configuration.nix
 sudo NIX_CONFIG='experimental-features = nix-command flakes' nixos-rebuild switch --flake .#nixos
-sudo nixos-rebuild boot --flake .#nixos
