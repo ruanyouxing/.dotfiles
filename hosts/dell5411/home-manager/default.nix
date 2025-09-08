6d44badaa56723706969b073fd8584c312921542
ruanyouxing <ruanyouxing7@gmail.com>
2025-09-08 20:58:36 +0700

    natural scroll

--- hosts/dell5411/home-manager/default.nix
+++ hosts/dell5411/home-manager/default.nix
@@ -1,8 +1,11 @@
 { lib, ... }:
 {
-  wayland.windowManager.hyprland.extraConfig = ''
-    monitor=eDP-1,1920x1080@60,0x0,1.2
-  '';
+  wayland.windowManager.hyprland = {
+    settings.input.touchpad.natural_scroll = lib.mkForce true;
+    extraConfig = ''
+      monitor=eDP-1,1920x1080@60,0x0,1.2
+    '';
+  };
   home.shellAliases = {
     nixos-switch = lib.mkForce "nh os switch --ask --hostname dell5411 /home/hungz/.dotfiles";
     nixos-boot = lib.mkForce "nh os boot --ask --hostname dell5411 /home/hungz/.dotfiles";
