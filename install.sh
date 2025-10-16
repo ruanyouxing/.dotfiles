1f4da7c79dd2d1282cdfedfe589abf3c1032a60a
ruanyouxing <ruanyouxing7@gmail.com>
2025-10-16 16:44:36 +0700

    change install script

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ ! -d "${HOME}/.config/nvim" ]; then
-  git clone https://github.com/ruanyouxing/nvim ~/.config/nvim 
-fi
-
-nixos-generate-config --show-hardware-config > ./nixos/hardware-configuration.nix
-sudo NIX_CONFIG='experimental-features = nix-command flakes' nixos-rebuild switch --flake .#nixos
