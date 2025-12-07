f8fb594838801ef334b120d50e707e744b06608c
ruanyouxing <ruanyouxing7@gmail.com>
2025-12-07 18:20:58 +0700

    flake and submodules

--- modules/home-manager/tools/neovim.nix
+++ modules/home-manager/tools/neovim.nix
@@ -1,39 +1,12 @@
-{
-  inputs,
-  pkgs,
-  ...
-}:
+{ inputs, ... }:
 let
   nvim-config = inputs.nvim-config;
 in
 {
-  home.packages = with pkgs; [
-    gcc
-    gnumake
-    fd
-    fzf
-    ripgrep
-    lazygit
-    luajit
-    nodejs
-    yarn
-    python311Packages.pip
-    haskellPackages.greenclip
-    eslint_d
-    nodePackages.prettier
-    clang-tools
-    ccls
-    lldb
-    python310Packages.debugpy
-    black
-    stylua
-    shellcheck
-    alejandra
-  ];
   programs.neovim = {
     enable = true;
     defaultEditor = true;
-    package = pkgs.neovim-unwrapped;
   };
   xdg.configFile."nvim".source = nvim-config;
+  home.packages = nvim-config.utils;
 }
