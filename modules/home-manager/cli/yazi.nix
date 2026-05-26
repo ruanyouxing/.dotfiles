3e9f3ca6f14654fc0ce86dcae90f3ec920c1331b
ruanyouxing <ruanyouxing7@gmail.com>
2026-05-27 00:50:17 +0700

    h

--- modules/home-manager/cli/yazi.nix
+++ modules/home-manager/cli/yazi.nix
@@ -4,24 +4,72 @@
     enableZshIntegration = true;
     settings = {
       opener = {
+        edit = [
+          {
+            run = "nvim %s";
+            block = true;
+          }
+        ];
+        view-image = [
+          {
+            run = "ristretto \"$@\"";
+            desc = "View image";
+          }
+        ];
+        view-video = [
+          {
+            run = "mpv \"$@\"";
+            desc = "Play video";
+          }
+        ];
+        play-audio = [
+          {
+            run = "mpv \"$@\"";
+            desc = "Play audio";
+          }
+        ];
         view-pdf = [
           {
             run = "zathura \"$@\"";
             desc = "View PDF";
           }
         ];
-        edit = [
+        browse = [
           {
-            run = "$EDITOR %s";
-            block = true;
+            run = "brave \"$@\"";
+            desc = "Open in Brave";
           }
         ];
       };
       open = {
         rules = [
           {
+            mime = "text/html";
+            use = "browse";
+          }
+          {
             mime = "text/*";
             use = "edit";
+          }
+          {
+            mime = "image/*";
+            use = "view-image";
+          }
+          {
+            mime = "video/*";
+            use = "view-video";
+          }
+          {
+            mime = "audio/*";
+            use = "play-audio";
+          }
+          {
+            mime = "application/pdf";
+            use = "view-pdf";
+          }
+          {
+            mime = "application/epub+zip";
+            use = "view-pdf";
           }
         ];
       };
