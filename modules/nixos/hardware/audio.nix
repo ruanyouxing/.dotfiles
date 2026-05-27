23486a77648406e9eed0eb7283c098785c39b6e4
ruanyouxing <ruanyouxing7@gmail.com>
2026-05-27 12:02:26 +0700

    bluetooth audio fix

--- modules/nixos/hardware/audio.nix
+++ modules/nixos/hardware/audio.nix
@@ -5,6 +5,16 @@
     alsa-utils
   ];
   security.rtkit.enable = true;
+
+  boot.extraModprobeConfig = ''
+    options iwlwifi bt_coex_active=0
+    options iwlwifi swcrypto=1
+    options iwlwifi power_save=0
+    options iwlwifi uapsd_disable=1
+    options iwlwifi d0i3_disable=1
+    options iwlmvm power_scheme=1
+  '';
+
   hardware = {
     enableAllFirmware = true;
     bluetooth = {
@@ -12,7 +22,12 @@
       powerOnBoot = true;
       settings = {
         General = {
+          ControllerMode = "bredr";
           Experimental = true;
+          FastConnectable = true;
+        };
+        Policy = {
+          AutoEnable = true;
         };
       };
     };
